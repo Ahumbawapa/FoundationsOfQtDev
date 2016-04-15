726dd4c7a6b0f422831f47ce508ecc924756dc11
Karsten Kleine <k.kleine@kk2001.net>
2016-04-15 20:46:23 +0200

    Exchange std:cout with qDebug() and use of QString instead of std::string

--- chap01/main.cpp
+++ chap01/main.cpp
@@ -1,23 +1,22 @@
-#include <iostream>
+#include <QtDebug>
+#include <QObject>
+#include <QString>
 #include "myclass.h"
 
 int main(int argc, char **argv)
 {
+    QObject parent; //is responsible for deleting the child items
     MyClass *a, *b, *c;
 
     a = new MyClass("foo");
     b = new MyClass("ba-a-ar");
     c = new MyClass("baz");
 
-    std::cout << a->text() << "(" << a->getLengthOfText() << ")" << std::endl;
+    qDebug() << QString::fromStdString(a->text()) << "(" << a->getLengthOfText() << ")";
     a->setText(b->text());
-    std::cout << a->text() << "(" << a->getLengthOfText() << ")" << std::endl;
+    qDebug() << QString::fromStdString(a->text()) << "(" << a->getLengthOfText() << ")" << std::endl;
 
     int result = a->getLengthOfText() - c->getLengthOfText();
-
-    delete a;
-    delete b;
-    delete c;
 
     return result;
 }
