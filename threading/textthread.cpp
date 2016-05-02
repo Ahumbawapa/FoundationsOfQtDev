edd62756285bd92f15db44ba7fbc15ab76d07f5e
Karsten Kleine <k.kleine@inka-sys.de>
2016-05-02 14:31:04 +0200

    second thread example with mutex to lock qDebug

--- threading/textthread.cpp
+++ threading/textthread.cpp
@@ -1,9 +1,10 @@
 #include "textthread.h"
 #include <QDebug>
+#include <QMutexLocker>
 
 //global variable used to stop execution of all threads
 bool stopThreads = false;
-
+QMutex mutex;
 
 TextThread::TextThread(const QString &text)
     :QThread()//important to call base class constructor for valid initialization
@@ -16,7 +17,15 @@ void TextThread::run()
     //send text to debug output until stopThreads is true
     while(!stopThreads)
     {
+        mutex.lock();
+        if(stopThreads)
+        {
+            mutex.unlock();
+            return;
+        }
+
         qDebug() << m_text;
         sleep(1);
+        mutex.unlock();
     }
 }
